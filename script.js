68edf2eadd28e4673adff575fa6064d6886bd023
gustavogualbertodasilva <gustavogualbertodasilva04@gmail.com>
2024-08-19 17:10:24 -0300

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var dica = window.document.getElementById("dica")
 var input = window.document.getElementById("input")
 var numero = Math.floor(Math.random() * 100) + 1
 var botao = window.document.getElementsByClassName("botao")
+var all = window.document.getElementById("all")
 var pontos = 0
 input.value = ""
 dica.value = ("ADIVINHE O NUMERO DE 1 A 100")
@@ -19,6 +20,7 @@ if (input.value <= 0 ||input.value >= 101){
     dica.value = ("USE UM NÚMERO ENTRE 1 E 100")
 }
 if(input.value == numero){
+    all.style.boxShadow = `0px 15px 50px rgb(9, 121, 52)`
     pontos += 1
     dica.value = "PARABÉNS VOCÊ ACERTOU"
     botao[0].disabled = true
@@ -36,12 +38,13 @@ if(input.value == numero){
             }
             if(input.value < numero) {
                 dica.value = ("TENTE UM NUMERO MAIOR")
-                dica.style.backgroundColor = `rgb(52, 173, 137)`
+                dica.style.backgroundColor = `rgb(72, 74, 185)`
             }
     }
 }
 }
 function reset(){
+    botao[1].style.boxShadow = `0px 15px 50px rgb(35, 32, 211)`
     botao[1].style.filter = 'brightness(120%)';
     setTimeout(function() {
     botao[1].style.filter = 'brightness(70%)';
